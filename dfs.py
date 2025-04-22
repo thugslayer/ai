b33cbafc8b6d1ac85e99f92bbcb221a4141e09dd
thugslayer <163440020+thugslayer@users.noreply.github.com>
2025-04-22 21:31:42 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -13,3 +13,12 @@ def dfs(graph, start, n):
                     stack.append(neighbor)
 
     return path
+
+n = int(input("Number of nodes: "))
+print("Enter adjacency matrix:")
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+start = int(input("Source node: ")) - 1
+result = dfs(graph, start, n)
+
+print("DFS Traversal Path:", result)
