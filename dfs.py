b2715ac005efcb9da01b17db5b98c0632d87277d
thugslayer <163440020+thugslayer@users.noreply.github.com>
2025-04-22 09:23:51 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,24 +1,15 @@
-def dfs(node, visited, adj_matrix, dfs_order):
-    visited[node] = True
-    dfs_order.append(node)
+def dfs(graph, start, n):
+    visited = [False] * n
+    stack = [start]
+    path = []
 
-    for neighbor in range(1, len(adj_matrix)):
-        if adj_matrix[node][neighbor] == 1 and not visited[neighbor]:
-            dfs(neighbor, visited, adj_matrix, dfs_order)
+    while stack:
+        node = stack.pop()
+        if not visited[node]:
+            visited[node] = True
+            path.append(node + 1)
+            for neighbor in reversed(range(n)):
+                if graph[node][neighbor] == 1 and not visited[neighbor]:
+                    stack.append(neighbor)
 
-n = int(input("Enter no. of nodes: "))
-print("Enter adjacency matrix:")
-adj_matrix = [[0] * (n + 1)]
-
-for _ in range(n):
-    row = [0] + list(map(int, input().split()))
-    adj_matrix.append(row)
-
-start_node = int(input("Enter start node: "))
-
-# DFS
-visited = [False] * (n + 1)
-dfs_order = []
-dfs(start_node, visited, adj_matrix, dfs_order)
-
-print("DFS Traversal Order:", dfs_order)
+    return path
